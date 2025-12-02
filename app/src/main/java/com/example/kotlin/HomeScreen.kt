2dbb514495a96243609bf665d2e478decbba22ba
Nelish Maharjan <nelishmaharjan16@icloud.com>
2025-12-02 09:23:59 +0545

    yeah

--- app/src/main/java/com/example/kotlin/HomeScreen.kt
+++ app/src/main/java/com/example/kotlin/HomeScreen.kt
@@ -1,92 +1,108 @@
 package com.example.kotlin
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun HomeScreen() {
-    Column(
+    var data = listOf(
+        R.drawable.hi1,
+        R.drawable.h2,
+        R.drawable.hi3,
+        R.drawable.hi1,
+        R.drawable.hi1,
+        R.drawable.hi1,
+        R.drawable.hi1
+    )
+    LazyColumn(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.Blue),
+            .background(Color.Yellow),
 
         ) {
-        Button(onClick = {}){
-            Text("Click me")
-        }
-        Button(onClick = {}){
-            Text("Click me")
-        }
-        Button(onClick = {}){
-            Text("Click me")
-        }
-        Button(onClick = {}){
-            Text("Click me")
-        }
-        Button(onClick = {}){
-            Text("Click me")
-        }
-        Button(onClick = {}){
-            Text("Click me")
-        }
-        Button(onClick = {}){
-            Text("Click me")
-        }
-        Button(onClick = {}){
-            Text("Click me")
-        }
-        Button(onClick = {}){
-            Text("Click me")
-        }
-        Button(onClick = {}){
-            Text("Click me")
-        }
-        Button(onClick = {}){
-            Text("Click me")
-        }
-        Button(onClick = {}){
-            Text("Click me")
-        }
-        Button(onClick = {}){
-            Text("Click me")
-        }
-        Button(onClick = {}){
-            Text("Click me")
-        }
-        Button(onClick = {}){
-            Text("Click me")
-        }
-        Button(onClick = {}){
-            Text("Click me")
-        }
-        Button(onClick = {}){
-            Text("Click me")
-        }
-        Button(onClick = {}){
-            Text("Click me")
-        }
-        Button(onClick = {}){
-            Text("Click me")
+        item {
+            Text("Welcome, Good morning ")
         }
 
-        Button(onClick = {}){
-            Text("Click me")
+        item {
+            LazyRow {
+                items(data.size){index->
+                    Image(
+                        painter = painterResource(data[index]),
+                        contentDescription = null,
+                        modifier = Modifier.size(70.dp).padding(end = 10.dp)
+                    )
+                }
+            }
         }
-        Button(onClick = {}){
-            Text("Click me")
+
+        item{
+            Spacer(modifier = Modifier.height(15.dp))
+            Image(
+                painter = painterResource(R.drawable.hi3),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.fillMaxWidth().height(100.dp).padding(end = 10.dp)
+            )
         }
-        Button(onClick = {}){
-            Text("Click me")
+
+        item {
+            LazyRow {
+                items(data.size){index->
+                    Image(
+                        painter = painterResource(data[index]),
+                        contentDescription = null,
+                        modifier = Modifier.size(70.dp).padding(end = 10.dp)
+                    )
+                }
+            }
         }
-        Button(onClick = {}){
-            Text("Click me")
+        items(data.size){index->
+            Image(
+                painter = painterResource(data[index]),
+                contentDescription = null,
+                modifier = Modifier.size(70.dp).padding(end = 10.dp)
+            )
+        }
+
+        item {
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2),
+                horizontalArrangement = Arrangement.spacedBy(10.dp),
+                verticalArrangement = Arrangement.spacedBy(10.dp),
+                modifier = Modifier
+                    .height(500.dp)
+                    .fillMaxWidth()
+            ) {
+                items(data.size){index->
+                    Image(
+                        painter = painterResource(data[index]),
+                        contentDescription = null,
+                        modifier = Modifier.size(70.dp).padding(end = 10.dp)
+                    )
+                }
+            }
         }
     }
 }
