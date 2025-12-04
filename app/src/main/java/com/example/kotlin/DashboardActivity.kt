44cfa8fee0921f0b190f182e79d0221c7d91ca93
Nelish Maharjan <nelishmaharjan16@icloud.com>
2025-12-04 07:48:32 +0545

    firebase update

--- app/src/main/java/com/example/kotlin/DashboardActivity.kt
+++ app/src/main/java/com/example/kotlin/DashboardActivity.kt
@@ -1,13 +1,11 @@
 package com.example.kotlin
 
 import android.app.Activity
-import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CenterAlignedTopAppBar
@@ -18,7 +16,6 @@ import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -28,8 +25,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.kotlin.R
 import com.example.kotlin.ui.theme.Blue
 import com.example.kotlin.ui.theme.White
 
