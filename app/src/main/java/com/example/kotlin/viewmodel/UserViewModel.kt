71dee35559d8cf715d0f20f25216889e1d43de6f
Nelish Maharjan <nelishmaharjan16@icloud.com>
2025-12-09 08:29:05 +0545

    helo

--- app/src/main/java/com/example/kotlin/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/example/kotlin/viewmodel/UserViewModel.kt
@@ -8,24 +8,41 @@ import com.google.firebase.auth.FirebaseUser
 class UserViewModel (val repo: UserRepo) : ViewModel(){
     fun login(email: String, password: String,
               callback: (Boolean, String?) -> Unit)
+    {
+
+    }
 
     fun register(email: String, password: String,
                  callback: (Boolean, String, String) -> Unit)
+    {
 
+    }
     fun addUserToDatabase(userId : String,
                           callback: (Boolean , String) -> Unit)
+    {
 
+    }
     fun getUserById(userId: String,
                     callback: (Boolean, UserModel) -> Unit)
+    {
 
+    }
     fun getAllUser(callback: (Boolean, List<UserModel>) -> Unit)
+    {
 
+    }
     fun getCurrentUser() : FirebaseUser ?
+    {
 
+    }
     fun deleteUser(userId: String,
                    callback: (Boolean, String) -> Unit)
+    {
 
+    }
     fun updateProfile(userId: String, model: UserModel,
                       callback: (Boolean, String) -> Unit)
-}
+    {
+
+    }
 }
